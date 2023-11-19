87f1879f3f227e628a08a3f0d30903fab978b233
danibetzamora <danielbetancor2000@gmail.com>
2023-11-19 22:59:04 +0000

    feat: ✨ Método para obtener datos del home desde Firestore

--- app/src/main/java/com/example/goshopapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/goshopapp/presentation/screens/home/HomeScreen.kt
@@ -1,26 +1,59 @@
 package com.example.goshopapp.presentation.screens.home
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import com.example.goshopapp.data.FirebaseFirestoreManage
+import com.example.goshopapp.domain.interfaces.HomePageDataCallback
+import com.example.goshopapp.domain.model.HomePageData
 
 @Composable
 fun HomeScreen() {
-    Column(modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ){
-        Text(text = "Home Screen")
+    val firestoreManager = FirebaseFirestoreManage()
+    var homeData by remember { mutableStateOf<HomePageData?>(null) }
+
+    DisposableEffect(Unit) {
+        firestoreManager.getHomePageData(object : HomePageDataCallback {
+            override fun onHomePageDataReceived(data: HomePageData) {
+                homeData = data
+            }
+            override fun onHomePageDataError(error: Exception) {
+                Log.d("Error", "Error al obtener datos: $error")
+            }
+        })
+
+        onDispose { }
+    }
+
+    homeData?.let { data ->
+        // Mostrar los nombres de los productos
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(data.products) { product ->
+                Text(text = product.name)
+
+                AsyncImage(
+                    model = product.image,
+                    contentDescription = "Translated description of what the image contains",
+                )
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+/*@Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
     HomeScreen()
-}
+}*/
