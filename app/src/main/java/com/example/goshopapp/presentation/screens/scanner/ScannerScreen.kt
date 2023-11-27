9e8d3745afe31c97951f5af1348e37d0cd3ec290
Pablo Santana <pablosnta14@hotmail.com>
2023-11-27 20:43:39 +0000

    Lector de códigos de barra terminado

--- app/src/main/java/com/example/goshopapp/presentation/screens/scanner/ScannerScreen.kt
+++ app/src/main/java/com/example/goshopapp/presentation/screens/scanner/ScannerScreen.kt
@@ -1,3 +1,5 @@
+package com.example.goshopapp.presentation.screens.scanner
+
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
@@ -12,11 +14,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color as ComposeColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,16 +32,26 @@ import com.google.zxing.integration.android.IntentIntegrator
 
 @Composable
 fun ScannerScreen() {
-    val buttonColor = Color(android.graphics.Color.parseColor("#007562"))
+    val buttonColor = ComposeColor(android.graphics.Color.parseColor("#007562"))
     val context = LocalContext.current
-
-    // State for showing the scanned value in the Snackbar
     var scannedValue by remember { mutableStateOf<String?>(null) }
+    var showDialog by remember { mutableStateOf(false) }
 
-    // Initialize the activity result launcher
+    // Inicializar el lanzador de resultados de la actividad de escaneo
     val launcher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         handleActivityResult(result.resultCode, result.data) { value ->
+            // Almacenar el valor escaneado y mostrar el diálogo
             scannedValue = value
+            showDialog = true
+        }
+    }
+
+    // Iniciar la cámara y escanear al entrar en la pantalla
+    LaunchedEffect(key1 = scannedValue) {
+        if (scannedValue == null && hasCameraPermission(context)) {
+            openCamera(context, launcher)
+        } else {
+            // Lógica después de aceptar el código de barras
         }
     }
 
@@ -46,45 +62,22 @@ fun ScannerScreen() {
     ) {
         Spacer(modifier = Modifier.padding(vertical = 16.dp))
 
-        Button(
-            onClick = {
-                if (hasCameraPermission(context)) {
-                    // Start the scanning activity
-                    openCamera(context, launcher)
-                } else {
-                    requestCameraPermission(context)
-                }
-            },
-            modifier = Modifier
-                .wrapContentWidth()
-                .padding(top = 16.dp, bottom = 16.dp),
-            colors = ButtonDefaults.buttonColors(buttonColor),
-            shape = RoundedCornerShape(10.dp)
-        ) {
-            Text(text = "Leer código de barras")
-        }
-
-        // Show Snackbar when there is a scanned value
-        scannedValue?.let { value ->
-            Snackbar(
-                modifier = Modifier.padding(16.dp),
-                action = {
-                    TextButton(onClick = { scannedValue = null }) {
-                        Text("OK")
-                    }
-                }
-            ) {
-                Text("Código de barras leído: $value")
+        // Mostrar el diálogo si showDialog es true
+        if (showDialog) {
+            ShowBarcodeDialog(scannedValue ?: "") {
+                // Reiniciar el valor escaneado y activar la cámara nuevamente
+                scannedValue = null
+                showDialog = false
             }
         }
     }
 }
 
-// Updated openCamera function to use the launcher
+// Función para abrir la cámara y escanear el código de barras
 private fun openCamera(context: Context, launcher: ActivityResultLauncher<Intent>) {
     val integrator = IntentIntegrator(context as Activity)
-        .setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
-        .setPrompt("Escanea un código de barras")
+        .setDesiredBarcodeFormats(IntentIntegrator.EAN_13)
+        .setPrompt("Escanea tu producto")
         .setOrientationLocked(false)
         .setBeepEnabled(true)
         .createScanIntent()
@@ -92,14 +85,17 @@ private fun openCamera(context: Context, launcher: ActivityResultLauncher<Intent
     launcher.launch(integrator)
 }
 
-// Handle the result of the scanning activity
+// Función para verificar si se otorgó el permiso de cámara
+private fun hasCameraPermission(context: Context): Boolean {
+    return ContextCompat.checkSelfPermission(context, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+}
+
+// Función para manejar el resultado de la actividad de escaneo
 private fun handleActivityResult(resultCode: Int, data: Intent?, scannedValueCallback: (String) -> Unit) {
     val result = IntentIntegrator.parseActivityResult(resultCode, data)
     if (result != null) {
         if (result.contents != null) {
-            // Handle the scanned barcode value here
             val barcodeValue = result.contents
-            // Show the scanned value in the Snackbar
             scannedValueCallback(barcodeValue)
         } else {
             // Handle case where scanning was canceled or failed
@@ -108,22 +104,27 @@ private fun handleActivityResult(resultCode: Int, data: Intent?, scannedValueCal
     }
 }
 
-// Function to verify if the camera permission is granted
-private fun hasCameraPermission(context: Context): Boolean {
-    return ContextCompat.checkSelfPermission(context, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
-}
-
-// Function to request camera permission
-private fun requestCameraPermission(context: Context) {
-    ActivityCompat.requestPermissions(
-        context as Activity,
-        arrayOf(android.Manifest.permission.CAMERA),
-        CAMERA_PERMISSION_REQUEST_CODE
-    )
-}
-
 // Código de solicitud para el permiso de la cámara
 private const val CAMERA_PERMISSION_REQUEST_CODE = 1001
+
+// Mostrar el diálogo con el valor del código de barras
+@Composable
+fun ShowBarcodeDialog(barcodeValue: String, onClose: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = { onClose() },
+        title = { Text("Código de Barras") },
+        text = { Text("Valor: $barcodeValue") },
+        confirmButton = {
+            Button(
+                onClick = {
+                    onClose()
+                }
+            ) {
+                Text("Aceptar")
+            }
+        }
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
