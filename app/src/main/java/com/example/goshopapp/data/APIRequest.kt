b59600ceb66d9c8679fa8d6c0b53c37b03bd19d7
Pablo Santana <pablosnta14@hotmail.com>
2023-12-10 14:17:27 +0000

    API response fail

--- app/src/main/java/com/example/goshopapp/data/APIRequest.kt
+++ app/src/main/java/com/example/goshopapp/data/APIRequest.kt
@@ -5,75 +5,95 @@ import com.example.goshopapp.domain.model.Product
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.json.JSONObject
+import java.net.SocketTimeoutException
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 class APIRequest(val productId: String) {
     fun execute(): Product {
-        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-        StrictMode.setThreadPolicy(policy)
+            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
 
-        val request = Request.Builder()
-            .url("https://world.openfoodfacts.net/api/v2/product/$productId?fields=product_name,ingredients_text,image_url")
-            .build()
-        val response = OkHttpClient().newCall(request).execute()
-        if (response.isSuccessful) {
-            val jsonObject = JSONObject(response.body!!.string())
-            val productData = jsonObject.getJSONObject("product")
+            val client = OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS)
+                .build()
 
-            // Comprobar si el campo nombre existe
-            val productName = if (productData.has("product_name")) {
-                productData["product_name"].toString().trim().replace(Regex("\\s+"), " ").replace(Regex("[^a-zA-Z0-9\\s]"), "")
+            val request = Request.Builder()
+                .url("https://world.openfoodfacts.net/api/v2/product/$productId?fields=product_name,ingredients_text,image_url")
+                .build()
+        try {
+            val response = client.newCall(request).execute()
+            if (response.isSuccessful) {
+                val jsonObject = JSONObject(response.body!!.string())
+                val productData = jsonObject.getJSONObject("product")
+
+                // Comprobar si el campo nombre existe
+                val productName = if (productData.has("product_name")) {
+                    productData["product_name"].toString().trim().replace(Regex("\\s+"), " ").replace(Regex("[^a-zA-Z0-9\\s]"), "")
+                } else {
+                    "product not found"
+                }
+
+                // Comprobar si el campo imagen existe
+                val productImage = if (productData.has("image_url")) {
+                    productData["image_url"].toString()
+                } else {
+                    "https://firebasestorage.googleapis.com/v0/b/goshopapp-f2548.appspot.com/o/Iconos%2Fplaceholder.png?alt=media&token=c0067f0d-2242-4bc6-a224-3dc0643933df"
+                }
+
+                // Comprobar si el campo ingredientes existe
+                var ingredients = if (productData.has("ingredients_text")) {
+                    productData["ingredients_text"].toString()
+                } else {
+                    ""
+                }
+
+                // Comprobar si los ingredientes están vacíos
+                if (ingredients == "") {
+                    ingredients = "Ingredientes no disponibles para este producto"
+                }
+
+                // Si los ingredientes exceden los caracteres máximos...
+                if (ingredients.length >= 250) {
+                    ingredients = ingredients.substring(0, 250).trim() + "..."
+                }
+
+                // Obtención del precio
+                val numeroDouble = Random.nextDouble(0.0, 4.0)
+                val numeroString = "%.2f".format(numeroDouble)
+
+                return Product(
+                    // Nombre
+                    productName.ifEmpty { "product not found" },
+                    // Descripción (Temporalmente es el nombre)
+                    productName.ifEmpty { "product not found" },
+                    // Ingredientes (Campo information del producto en BDD)
+                    ingredients,
+                    // Precio (Random)
+                    numeroString.replace(",", "."),
+                    // Imagen
+                    productImage.ifEmpty { "https://firebasestorage.googleapis.com/v0/b/goshopapp-f2548.appspot.com/o/Iconos%2Fplaceholder.png?alt=media&token=c0067f0d-2242-4bc6-a224-3dc0643933df" }
+                )
             } else {
-                "product not found"
+                // Si la respuesta no es exitosa, asignar "API response fail" a todos los campos
+                return Product(
+                    "product not found",
+                    "product not found",
+                    "product not found",
+                    "product not found",
+                    "product not found"
+                )
             }
-
-            // Comprobar si el campo imagen existe
-            val productImage = if (productData.has("image_url")) {
-                productData["image_url"].toString()
-            } else {
-                "https://firebasestorage.googleapis.com/v0/b/goshopapp-f2548.appspot.com/o/Iconos%2Fplaceholder.png?alt=media&token=c0067f0d-2242-4bc6-a224-3dc0643933df"
-            }
-
-            // Comprobar si el campo ingredientes existe
-            var ingredients = if (productData.has("ingredients_text")) {
-                productData["ingredients_text"].toString()
-            } else {
-                ""
-            }
-
-            // Comprobar si los ingredientes están vacíos
-            if (ingredients == "") {
-                ingredients = "Ingredientes no disponibles para este producto"
-            }
-
-            // Si los ingredientes exceden los caracteres máximos...
-            if (ingredients.length >= 250) {
-                ingredients = ingredients.substring(0,250).trim() + "..."
-            }
-
-            // Obtención del precio
-            val numeroDouble = Random.nextDouble(0.0, 4.0)
-            val numeroString = "%.2f".format(numeroDouble)
-
+        } catch (e: SocketTimeoutException) {
+            // Retornar un producto que indica el fallo de la API
             return Product(
-                // Nombre
-                productName.ifEmpty { "product not found" },
-                // Descripción (Temporalmente es el nombre)
-                productName.ifEmpty { "product not found" },
-                // Ingredientes (Campo information del producto en BDD)
-                ingredients,
-                // Precio (Random)
-                numeroString.replace(",","."),
-                // Imagen
-                productImage.ifEmpty { "https://firebasestorage.googleapis.com/v0/b/goshopapp-f2548.appspot.com/o/Iconos%2Fplaceholder.png?alt=media&token=c0067f0d-2242-4bc6-a224-3dc0643933df" }
-            )
-        } else {
-            return Product(
-                "product not found",
-                "product not found",
-                "product not found",
-                "product not found",
-                "product not found"
+                "API response failed",
+                "API response failed",
+                "API response failed",
+                "API response failed",
+                "API response failed"
             )
         }
     }
